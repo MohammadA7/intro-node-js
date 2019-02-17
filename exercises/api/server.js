f15152b61e4c03a2abd8006e0682fe79caa07db2
Mohammad Alhomaid <mohammad.a.7@outlook.com>
2019-02-17 22:19:56 +0300

    fix server to look file async and return 404 if not found

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -9,7 +9,16 @@ const path = require('path')
  */
 const findAsset = (name) => {
   const assetPath = path.join(__dirname, 'assets', name)
-  return fs.readFileSync(assetPath, {encoding: 'utf-8'}).toString()
+  return new Promise((resolve, reject) => {
+    fs.readFile(assetPath, (error, data) => {
+      if (data) {
+        resolve(data)
+      } else {
+        reject(error)
+      }
+    })
+  })
+
 }
 
 const hostname = '127.0.0.1'
@@ -23,14 +32,32 @@ const server = http.createServer((req, res) => {
   const route = url.parse(req.url).pathname
   // this is sloppy, espcially with more assets, create a "router"
   if (route === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
-    logRequest(method, route, 200)
-    res.end()
+    findAsset('index.html')
+      .then(asset => {
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        })
+        res.write(asset.toString())
+        logRequest(method, route, 200)
+        res.end()
+      })
+      .catch(error => console.log(error))
   } else {
-    // missing asset should not cause server crash
-    throw new Error('route not found')
-    res.end()
+    findAsset(route.substr(1))
+      .then(asset => {
+        res.writeHead(200, {
+          'Content-Type': 'text/css'
+        })
+        res.write(asset.toString())
+        logRequest(method, route, 200)
+        res.end()
+      })
+      .catch(error => {
+        res.writeHead(404)
+        res.write(error.toString())
+        logRequest(method, route, 404)
+        res.end()
+      })
   }
   // most important part, send down the asset
 })
