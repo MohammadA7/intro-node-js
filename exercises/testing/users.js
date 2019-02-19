b2ce0f7b671ffde5e83b744b61a4fdce34382caf
Mohammad Alhomaid <mohammad.a.7@outlook.com>
2019-02-19 21:04:14 +0300

    add fixId function and test for it

--- exercises/testing/users.js
+++ exercises/testing/users.js
@@ -7,8 +7,11 @@ const users = new Array(20).fill(0)
   }
 })
 
+const fixId = (id) => parseInt(id)
+
 // simulate async db call with promise
 const findUser = (id) => new Promise((resolve, reject) => {
+  const id = fixId(id)
   const user = users.find(user => user.id === id)
   if (user) {
     return resolve(user)
@@ -20,16 +23,15 @@ const findUser = (id) => new Promise((resolve, reject) => {
 const deleteUser = (id) => new Promise((resolve, reject) => {
   const id = fixId(id)
   const i = users.findIndex(user => user.id === id)
-
   if (i < 0) {
     return reject(new Error(`No user with id "${id}"`))
   }
-
   users.slice(i, 1)
   resolve({id})
 })
 
 module.exports = {
   findUser,
-  deleteUser
+  deleteUser,
+  fixId
 }
